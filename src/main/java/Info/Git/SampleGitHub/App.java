1fd5915e85fef5e5b641ad5ea04ac55b5d5eb607
dtpsa <dtpsa@GSK-PC>
2019-09-19 10:47:42 +0530

    Initial Commit

---
+++ src/main/java/Info/Git/SampleGitHub/App.java
@@ -0,0 +1,13 @@
+package Info.Git.SampleGitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Info/Git/SampleGitHub/Test.java
@@ -0,0 +1,9 @@
+package Info.Git.SampleGitHub;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Git Repository Remote");
+	}
+
+}
